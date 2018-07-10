70c743679c3a37ddbc2773238d210354c132f7c6
James Thorpe <james.thorpe@utexas.edu>
2018-07-09 20:05:45 -0400

    added monte_carlo module

---
+++ src/monte_carlo.f90
@@ -0,0 +1,22 @@
+!/////////////////////////////////////////////////////////////////////
+!//             Module containing Monte Carlo Integration Subroutines
+!//
+!//                     James H Thorpe, in Group of John Stanton
+!//                     The University of Florida
+!//
+!/////////////////////////////////////////////////////////////////////
+
+! GENERAL STRUCTURE:
+
+! USER DEFINED HASH FUNCTIONS
+!       user must define a function that is passed in for the args
+!       This is referenced through the interface
+
+MODULE monte_carlo 
+  USE dynamic_arrays
+
+  CONTAINS
+
+  
+END MODULE monte_carlo
+
--- src/myUtils.f90
+++ src/myUtils.f90
@@ -1,6 +1,7 @@
 MODULE myUtils
   USE dynamic_arrays
   USE hash_tables
+  USE monte_carlo
   USE hello
  
   IMPLICIT NONE
